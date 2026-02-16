fe54fd7ba365a96a4e3a5c4fda392d52a3b09c96
mg-debugg <tidysky@gmail.com>
2026-02-16 10:17:24 +0900

    fix: 404 on invalid content id

--- src/app/tv/[id]/page.tsx
+++ src/app/tv/[id]/page.tsx
@@ -1,7 +1,9 @@
 import { ContentDetail } from '@/components/ContentDetail';
+import { notFound } from 'next/navigation';
 
 export default function TvDetailPage({ params }: { params: { id: string } }) {
   const { id } = params;
   const num = Number(id);
-  return <ContentDetail media_type="tv" id={Number.isFinite(num) ? num : 0} />;
+  if (!Number.isFinite(num) || num <= 0) notFound();
+  return <ContentDetail media_type="tv" id={num} />;
 }
